659a38c9d2dc4bbd3560efc3e21bf6b5545cda03
19HS-Studio <19heavenstreet@gmail.com>
2024-05-20 17:21:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ showBtn.addEventListener('click', function(){
         topNav.classList.add('showNav');
         showBtn.innerHTML = '<i class = "fas fa-times"></i>';
     }
+    document.addEventListener('DOMContentLoaded', function() {
+    // Ambil elemen judul website
+    var homeLink = document.getElementById('homeLink');
+    
+    // Tambahkan event listener untuk klik
+    homeLink.addEventListener('click', function() {
+        // Arahkan pengguna ke halaman utama
+        window.location.href = 'https://19hs-studio.github.io/';
+    });
+});
 });
 
 /* Lightbox */
